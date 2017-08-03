f4d9a8d84d7e61e57cf4f610860312a868d1651a
Rowen Scout <rowen.scout@gmail.com>
2017-08-02 17:10:05 -0700

    Constructor function renders to DOM now. Still working on tabs hiding/showing correctly.

--- scripts/app.js
+++ scripts/app.js
@@ -6,15 +6,36 @@ function Project(rawData) {
   this.title = rawData.title;
   this.description = rawData.description;
   this.imgSrc = rawData.imgSrc;
-  this.date = rawData.date;
-  projects.push(this);
+  this.date = rawData.publishedOn;
+  this.link = rawData.link;
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.find('h1').text(this.title);
+  $newProject.find('body').html(this.body);
+  $newProject.find('img').attr("src", this.image);
+  $newProject.find('span.pubdate').text(this.date);
+  $newProject.find('a.link').html(this.link);
+  $newProject.find('a.link').attr("href", this.link);
+  $newProject.find('article').attr("data-category", this.category);
+  $newProject.find('article').attr("id", "data");
+  return $newProject;
+}
+
+rawData.forEach(function(projectObj) {
+  projects.push(new Project(projectObj));
+});
+
+projects.forEach(function(project) {
+  $('#article').append(project.toHtml());
+});
+
 
 var handleNavBar = function() {
   $('section').on('click', function(){
     $('.data').hide();
     var dataCat = $(this).attr('data-category');
-    //$('".' + $(this).attr('data-category')) + '"'.show();
     $('.data[id="' + dataCat + '"]').show();
     console.log($(this).attr('data-category'));
   }
