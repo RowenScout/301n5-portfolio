58586c0af8ccfbde1d4458969d7d23f36bd45c92
Rowen Scout <rowen.scout@gmail.com>
2017-09-03 22:37:57 -0700

    added .reduce function in wordcount

--- scripts/controller.js
+++ scripts/controller.js
@@ -10,9 +10,23 @@ var repos = repos || [];
     console.log(response);
     response.map(eachRepo => {
       module.push(new Article(eachRepo))
-      module.forEach(function(input) {
-        $('#handlebars-entry').append(input.toHtml());
-      });
+    });
+    module.forEach(function(input) {
+      $('#handlebars-entry').append(input.toHtml());
     });
   });
 })(repos);
+
+var wordstring = repos.reduce(function(sum, word) {
+  return sum + word.ipsum;
+})
+
+//this method of counting words was written by github user bradoyler.
+//it can be found here https://gist.github.com/bradoyler/5610057
+//it's used here to count the number of words in each of my articles and return
+// and object with the wordcount to the console.
+var wordscount = wordstring.replace(/[^\w\s]/g, "").split(/\s+/).reduce(function(map, word){
+  map[word] = (map[word]||0)+1;
+  return map;
+}, Object.create(null))
+console.log(wordscount)
