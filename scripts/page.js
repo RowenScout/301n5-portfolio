b500b4e4230705c12a94d1872930d75bc2f9c807
Rowen Scout <rowen.scout@gmail.com>
2017-09-12 11:43:10 -0700

    added toHTML function, called in controller on each datapoint in localstorage. Added some notes in comments.

---
+++ scripts/page.js
@@ -0,0 +1,9 @@
+'use strict';
+const page = require('pagejs');
+page('/', index)
+page('/user/:user', show)
+page('/user/:user/edit', edit)
+page('/user/:user/album', album)
+page('/user/:user/album/sort', sort)
+page('*', notfound)
+page()
